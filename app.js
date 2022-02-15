57b9a8c0d51cfe858e9a5f3e42556f6eb6e81ee5
alamine95 <lsow313@gmail.com>
2022-02-15 14:39:29 +0000

    routes

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.get('/hello', (req, res) => {
 
 app.use('/api/v1/tasks', tasks)
 
+//app.get('/api/v1/tasks')  - get all the tasks 
+//app.post('/api/v1/tasks')  - create a new task
+//app.get('/api/v1/tasks/:id')  - get a single task
+//app.patch('/api/v1/tasks/:id')  - update task
+//app.delete('/api/v1/tasks/:id')  - delete task
+
+
 const port = 3000
 
 app.listen(port, console.log(`Server is listening on port ${port}...`))
